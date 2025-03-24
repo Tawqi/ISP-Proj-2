4d7ac8d042974c572c1a87cdb105217a4f52f20d
Tawqi <tawqitahmedchy@proton.me>
2025-03-24 07:44:46 +0600

    Initial commit

---
+++ Client/src/Components/Nav.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+function Nav() {
+
+    return (
+      <>
+        
+      </>
+    )
+  }
+  
+  export default Nav
---
+++ Client/src/Pages/Notfound.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+function Notfound() {
+
+  return (
+    <>
+      <h1 className="bg-red-600">This page does not exist</h1>
+    </>
+  )
+}
+
+export default Notfound
